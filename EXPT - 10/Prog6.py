b808932062fefda8bdd600e34025bdd7de09e82d
Ayushman2005-cmyk <24cseaiml003.ayushmankar@giet.edu>
2026-02-23 09:34:35 +0530

    23/02/2026

--- EXPT - 10/Prog6.py
+++ EXPT - 10/Prog6.py
@@ -4,8 +4,6 @@ class Number:
         self.value = value
     def __add__(self, other):
         return Number(self.value + other.value)
-    def __str__(self):
-        return str(self.value)
 num1 = Number(10)
 num2 = Number(20)
 result = num1 + num2
