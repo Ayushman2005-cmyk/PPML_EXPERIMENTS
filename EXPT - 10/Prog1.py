82496c8c2b9512412b59315a15bf961d02575f0b
Ayushman2005-cmyk <24cseaiml003.ayushmankar@giet.edu>
2026-02-23 09:03:47 +0530

    23/02/2026

--- EXPT - 10/Prog1.py
+++ EXPT - 10/Prog1.py
@@ -3,6 +3,8 @@ class Student:
     def child(self):
         print("Hello GIET")
 class S1(Student):
-    pass
+    def parent(self):
+        print("Hello Parent")
 s1 = S1()
 s1.child()
+s1.parent()
